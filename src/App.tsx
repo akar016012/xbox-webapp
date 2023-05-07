0c04a519bc9553cf6083be825103e80d9cac664f
Aditya Kar <61394999+akar016012@users.noreply.github.com>
2023-05-06 21:08:34 -0500

    Added form

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import { useState } from "react";
-import { Alert } from "./components/Alert";
-import { Button } from "./components/Button";
+import Form from "./components/Form";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
-      )}
-      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
+      <Form></Form>
     </div>
   );
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,29 @@
+import React, { FormEvent } from "react";
+
+const Form = () => {
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log("Submitted!");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input id="age" type="number" className="form-control" />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
