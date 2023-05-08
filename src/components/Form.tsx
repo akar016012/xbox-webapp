15eda4ee8a2ea525fbc185a37842a8c94d3b5335
Aditya Kar <61394999+akar016012@users.noreply.github.com>
2023-05-07 23:12:56 -0500

    Implementing ract hook form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,13 @@
-import React, { FormEvent } from "react";
-
+import React, { FormEvent, useState } from "react";
+import { useForm } from "react-hook-form";
 const Form = () => {
+  const form = useForm();
+  console.log(form);
+
+  const [person, setPerson] = useState({ name: "", age: 0 });
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    console.log("Submitted!");
+    console.log(person);
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -11,13 +15,26 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input type="text" className="form-control" />
+        <input
+          value={person.name}
+          onChange={(e) => setPerson({ ...person, name: e.target.value })}
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input
+          onChange={(e) =>
+            setPerson({ ...person, age: parseInt(e.target.value) })
+          }
+          value={person.age}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
